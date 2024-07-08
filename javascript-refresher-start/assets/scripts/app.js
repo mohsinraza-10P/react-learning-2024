b133003b604281c6e4fe133c7e2100649c5fc362
Mohsin Raza <mohsin.raza@10pearls.com>
2024-07-08 17:00:30 +0500

    Javascript refresher - import/export

---
+++ javascript-refresher-start/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// import apiKey from "./util"; -- Default import
+import { apiKey, abc as aliasExample } from "./util";
+import * as util from "./util";
+
+console.log(apiKey);
+console.log(util.abc);
+console.log(aliasExample);
---
+++ javascript-refresher-start/assets/scripts/util.js
@@ -0,0 +1,3 @@
+// export default "abcq1323dasfSDAF55"; -- Default export
+export let apiKey = "abcq1323dasfSDAF55";
+export let abc = "abc";
